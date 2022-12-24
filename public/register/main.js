461ae79387139185202904877ccfc1c3d46e72bc
suru-ach <srujanacharya36@gmail.com>
2022-12-24 08:58:14 +0530

    added register page

---
+++ public/register/main.js
@@ -0,0 +1,72 @@
+const user = document.querySelector('.user');
+const postEvent = document.querySelector('.postEvent');
+
+const alertDiv = document.querySelector('.alertDiv');
+
+const setLocalStorage = (username, token, role) => {
+    localStorage.setItem('user', username);
+    localStorage.setItem('token', `${token}`);
+    localStorage.setItem('role', role);
+}
+
+const setUser = () => {
+    const username = localStorage.getItem('user');
+    const role = localStorage.getItem('role');
+    user.innerHTML = username || 'guest';
+    postEvent.style.display = (role == 'admin') ? 'block' : 'none';
+}
+
+const register = document.querySelector('.register');
+const input_user = document.getElementById('username');
+const input_email = document.getElementById('email');
+const clubs = document.getElementById('clubs');
+const input_club = Array.from(clubs.querySelectorAll('input'));
+const input_password = document.getElementById('password');
+
+const submitData = async (e) => {
+    e.preventDefault();
+
+    alertDiv.innerHTML = 'loading';
+    alertDiv.classList.add('message');
+
+    const user = input_user.value;
+    const email = input_email.value;
+    const password = input_password.value;
+    const clubs = input_club.filter(val => val.checked).map(val => val.id.toUpperCase());
+    
+    try {           
+        const res = await axios.post('/user/register', {user, email, password, clubs});
+        if(res.status !== 201) {
+            throw Error(res);
+        }
+        alertDiv.innerHTML = 'account successfully created';
+        alertDiv.classList.remove('message');
+        alertDiv.classList.add('success');
+        register.reset();
+        setLocalStorage(res.data.username, res.data.token, res.data.role);
+        setUser();
+    } catch(err) {
+        alertDiv.innerHTML = err.response.data.msg;
+        alertDiv.classList.remove('message');
+        alertDiv.classList.add('error');
+    }
+    input_user.value = '';
+    input_email.value = '';
+    input_password.value = '';
+
+    setTimeout(() => {
+        alertDiv.classList.remove('error');
+        alertDiv.classList.remove('success');
+    }, 3000);
+}
+setUser();
+
+register.addEventListener('submit', (e) => submitData(e));
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
